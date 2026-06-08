4b40f5ab68e2accb2245f4a549e2e81e35cf7fcc
Dmytro <mail4dmytro@example.com>
2026-06-08 14:54:42 +0300

    Task 3: State-Based Debounce Using Level Verification

--- main/main.c
+++ main/main.c
@@ -1,13 +1,29 @@
  /*
- * Task 2: Software Debounce Using a Timer (Time-Based)
+ * Task 3: State-Based Debounce Using Level Verification
  *
- * Add time-based debounce logic:
- * - Ignore an interrupt if less than 50 ms has elapsed since the previous one.
- * - Implement the debounce logic outside the ISR.
+ * - The interrupt only signals that an event has occurred.
+ * - In the main loop() or task:
+ *   - Accept the event only if the button is still pressed.
+ *   - Ignore events generated when the button is released.
  *
  * Expected Result:
- * - Fewer false trigger events caused by button bouncing.
- * - Trigger events may still occur when the button is released.
+ * - Exactly one response for each button press.
+ * - Button release does not trigger any action.
+ * 
+ * Log example:
+ * 276) TASK2: Button GPIO: 18
+I (3406) TASK2: ISR#2, BUTTON_GPIO = 0, LED=1
+I (5316) TASK2: ISR#3, BUTTON_GPIO = 0, LED=0
+I (6296) TASK2: ISR#4, BUTTON_GPIO = 0, LED=1
+I (7386) TASK2: ISR#5, BUTTON_GPIO = 0, LED=0
+I (8396) TASK2: Ignored: button already released (bounce)
+I (8556) TASK2: ISR#7, BUTTON_GPIO = 0, LED=1
+I (12186) TASK2: ISR#8, BUTTON_GPIO = 0, LED=0
+I (13946) TASK2: ISR#9, BUTTON_GPIO = 0, LED=1
+I (15116) TASK2: ISR#10, BUTTON_GPIO = 0, LED=0
+I (16116) TASK2: ISR#12, BUTTON_GPIO = 0, LED=1
+I (16706) TASK2: Ignored: button already released (bounce)
+I (17546) TASK2: ISR#14, BUTTON_GPIO = 0, LED=0
  */
 
 #include <stdio.h>
@@ -19,7 +35,6 @@
 
 #define BUTTON_GPIO     GPIO_NUM_18   /* change to your pin */
 #define TAG             "TASK2"
-#define DEBOUNCE_TIME   150
 
 static volatile uint32_t isr_counter = 0;
 static volatile bool button_pressed = false;
@@ -55,25 +70,18 @@ void app_main(void)
     ESP_LOGI(TAG, "Task 1 started. Press the button and watch the counter.");
     ESP_LOGI(TAG,"Button GPIO: %d", BUTTON_GPIO);
 
-    uint64_t prev_time = 0;
     while (true) {
         if (button_pressed) {
-            
             button_pressed = false;
-            uint64_t now = esp_timer_get_time();
-            
-            if ((now - prev_time) > (DEBOUNCE_TIME * 1000ULL)) {
+            if (gpio_get_level(BUTTON_GPIO) == 0) { // accept event if button still pressed
                 uint32_t current = isr_counter;
                 led_state = !led_state;
                 gpio_set_level(GPIO_NUM_15, led_state);
 
-                ESP_LOGI(TAG,
-                     "ISR#%" PRIu32 ", BUTTON_GPIO = %d, debunce = %" PRIu64 "ms",
-                     current,
-                     gpio_get_level(BUTTON_GPIO),
-                     (now - prev_time) / 1000);
-            
-                prev_time = now;
+                ESP_LOGI(TAG, "ISR#%" PRIu32 ", BUTTON_GPIO = %d, LED=%d",
+                         current, gpio_get_level(BUTTON_GPIO), (int)led_state);
+            } else {
+                ESP_LOGI(TAG, "Ignored: button already released (bounce)");
             }
         }
             
